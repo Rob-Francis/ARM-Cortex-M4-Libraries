5252cf7e1334e039c894204a15b3861f40dcb203
csse3010 <robert.francis@uqconnect.edu.au>
2020-03-06 20:14:19 +1000

    LTA changes

--- lta1000g/s4532390_hal_lta1000g.c
+++ lta1000g/s4532390_hal_lta1000g.c
@@ -1,4 +1,5 @@
 #include "s4532390_hal_lta1000g.h"
+#include "debug_printf.h"
 
 #define NUM_PINS 10
 
@@ -15,7 +16,7 @@ uint8_t IOPins[] = {
         BRD_D25_PIN
     };
 
-uint8_t IOPorts[] = {
+int IOPorts[] = {
         BRD_D16_GPIO_PORT,
         BRD_D17_GPIO_PORT,
         BRD_D18_GPIO_PORT,
@@ -32,7 +33,17 @@ void s4532390_hal_lta1000g_init() {
 
     GPIO_InitTypeDef  GPIO_InitStructure;
     
-    
+    __BRD_D16_GPIO_CLK();
+    __BRD_D17_GPIO_CLK();
+    __BRD_D18_GPIO_CLK();
+    __BRD_D19_GPIO_CLK();
+    __BRD_D20_GPIO_CLK();
+    __BRD_D21_GPIO_CLK();
+    __BRD_D22_GPIO_CLK();
+    __BRD_D23_GPIO_CLK();
+    __BRD_D24_GPIO_CLK();
+    __BRD_D25_GPIO_CLK();
+
     
 
     for (int i = 0; i < NUM_PINS; ++i) {
@@ -42,16 +53,23 @@ void s4532390_hal_lta1000g_init() {
         GPIO_InitStructure.Pin = IOPins[i];				//Pin
         HAL_GPIO_Init(IOPorts[i], &GPIO_InitStructure);	//Initialise Pin
     }
+
+    // GPIO_InitStructure.Mode = GPIO_MODE_OUTPUT_PP;		//Output Mode
+  	//     GPIO_InitStructure.Pull = GPIO_PULLDOWN;			//Enable Pull up, down or no pull resister
+  	//     GPIO_InitStructure.Speed = GPIO_SPEED_FAST;			//Pin latency
+    //     GPIO_InitStructure.Pin = IOPins[0];				//Pin
+    //     HAL_GPIO_Init(IOPorts[0], &GPIO_InitStructure);	//Initialise Pin
   	
 
 };
 
 void lta1000g_seg_set(int segment, unsigned char segment_value) {
-    HAL_GPIO_WritePin(IOPorts[segment], IOPins[segment], 1 & segment);
+    HAL_GPIO_WritePin(IOPorts[segment], IOPins[segment], segment_value & 0x01);
 }
 
 void s4532390_hal_lta1000g_write(unsigned short value) {
     for (int i = 0; i < NUM_PINS; i++) {
-        lta1000g_seg_set(i, 1 & (value << i));
+        lta1000g_seg_set(i, 0x01 & (value >> i));
+        debug_printf("Value: %d\n\r", value);	
     }
 }
