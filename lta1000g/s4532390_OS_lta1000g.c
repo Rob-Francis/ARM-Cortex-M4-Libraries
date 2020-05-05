c6c16edec7a6d722b636f786264c28452b2f6108
Robert <s4532390@student.uq.edu.au>
2020-05-05 19:15:59 +1000

    Commenting

--- lta1000g/s4532390_OS_lta1000g.c
+++ lta1000g/s4532390_OS_lta1000g.c
@@ -1,32 +1,71 @@
+/**
+*********************
+*********************
+*@file mylib/lta1000g/s4532390_OS_lta1000g.c
+*@author Robert Francis - 45323906
+*@date 5/05/2020
+*@brief RTOS LTA100G library
+*REFERENCE:
+csse3010_stage4.pdf 
+*********************
+*********************
+*EXTERNAL FUNCTIONs
+*********************
+*********************
+void s4532390_Tasklta1000g(void);
+void s4532390_os_lta1000g_init(void);
+void s4532390_os_lta1000g_deinit(void);
+*********************
+*********************
+**/
+
 /* Includes ------------------------------------------------------------------*/
 #include "board.h"
 #include "processor_hal.h"
 #include "debug_printf.h"
 #include <string.h>
-
-/* Scheduler includes. */
 #include "FreeRTOS.h"
 #include "task.h"
 #include "queue.h"
 #include "semphr.h"
-
 #include "s4532390_hal_lta1000g.h"
+#include "s4532390_OS_lta1000g.h"
 
+/* Private define ------------------------------------------------------------*/
+#define LTA1000GTASK_PRIORITY					( tskIDLE_PRIORITY + 6 ) //Task priority
+#define LTA1000GTASK_STACK_SIZE	( configMINIMAL_STACK_SIZE * 2 ) //Task stack size
 
-unsigned short lightbarValue;
+/* Private variables ---------------------------------------------------------*/
+unsigned short lightbarValue; // Stores current value written into lightbar
 
 /**
-  * @brief  Receiver Task. Used to receive messages.
+*@brief Initialises the lightbar task
+*@param  None
+*@retval None
+*/
+void s4532390_os_lta1000g_init(void) {
+    xTaskCreate( (void *) &s4532390_Tasklta1000g, (const signed char *) "LTAG", LTA1000GTASK_STACK_SIZE, NULL, LTA1000GTASK_PRIORITY, NULL );
+
+}
+
+/**
+*@brief Deiitialises the lightbar task
+*@param  None
+*@retval None
+*/
+void s4532390_os_lta1000g_deinit(void) {
+
+}
+
+/**
+  * @brief  Lightbar task - writes the current lightbarvalue into the bar
   * @param  None
   * @retval None
   */
 void s4532390_Tasklta1000g(void) {
 
-
-	for (;;) {
-
-        
-		s4532390_hal_lta1000g_write(lightbarValue);
-        vTaskDelay(100);
-	}
+  	for (;;) {        
+	  	  s4532390_hal_lta1000g_write(lightbarValue);
+        vTaskDelay(10); // Delay of 10ms
+	  }
 }
--- lta1000g/s4532390_OS_lta1000g.h
+++ lta1000g/s4532390_OS_lta1000g.h
@@ -1,10 +1,36 @@
+/**
+*********************
+*********************
+*@file mylib/lta1000g/s4532390_OS_lta1000g.h
+*@author Robert Francis - 45323906
+*@date 5/05/2020
+*@brief RTOS LTA100G library
+*REFERENCE:
+csse3010_stage4.pdf 
+*********************
+*********************
+*EXTERNAL FUNCTIONs
+*********************
+*********************
+void s4532390_Tasklta1000g(void);
+void s4532390_os_lta1000g_init(void);
+void s4532390_os_lta1000g_deinit(void);
+*********************
+*********************
+**/
+
 #ifndef S4532390_OS_LTA1000G_H
 #define S4532390_OS_LTA1000G_H
 
-extern unsigned short lightbarValue;
 
-void s4532390_Tasklta1000g(unsigned short);
+/**GLOBALS**/
+extern unsigned short lightbarValue; // Stores current value written into lightbar
 
+void s4532390_Tasklta1000g(void);
+
+void s4532390_os_lta1000g_init(void);
+
+void s4532390_os_lta1000g_deinit(void);
 
 
 
